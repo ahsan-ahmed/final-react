d1d3294238fc85b4ac3b21e46c04cc55c4ff341e
ahsan-ahmed <ahsan_ahmed01@yahoo.com>
2018-10-15 15:06:57 +0500

    Updates

---
+++ precache-manifest.e3448c85048305e3351d70f620e82638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/final-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a82744962ede2066ebf3",
+    "url": "/final-react/static/js/runtime~main.a8274496.js"
+  },
+  {
+    "revision": "091a87994d6b48acdac8",
+    "url": "/final-react/static/js/main.091a8799.chunk.js"
+  },
+  {
+    "revision": "3d878eda2e148fabc2a0",
+    "url": "/final-react/static/js/1.3d878eda.chunk.js"
+  },
+  {
+    "revision": "091a87994d6b48acdac8",
+    "url": "/final-react/static/css/main.a2fdea5e.chunk.css"
+  },
+  {
+    "revision": "16057b78f9c2a6f1d7cc6dd81680b2bf",
+    "url": "/final-react/index.html"
+  }
+];
